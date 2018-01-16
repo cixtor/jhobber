f0ecaaf414f134deb56e773c2319614e3c9a958f
Cixtor <cixtords@gmail.com>
2018-01-16 09:00:00 -0800

    Modify navigation drawer, menu, and imagery

--- app/src/main/java/com/cixtor/jhobber/MainActivity.java
+++ app/src/main/java/com/cixtor/jhobber/MainActivity.java
@@ -84,17 +84,13 @@ public class MainActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
-            // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        if (id == R.id.nav_home) {
 
-        } else if (id == R.id.nav_slideshow) {
+        } else if (id == R.id.nav_map) {
 
-        } else if (id == R.id.nav_manage) {
+        } else if (id == R.id.nav_profile) {
 
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
+        } else if (id == R.id.nav_settings) {
 
         }
 
