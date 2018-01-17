16a4c1fcd62722676378dcea7d36164ccf0a27b5
Cixtor <cixtords@gmail.com>
2018-01-17 09:00:00 -0800

    Add fragment manager and navigation switcher

--- app/src/main/java/com/cixtor/jhobber/MainActivity.java
+++ app/src/main/java/com/cixtor/jhobber/MainActivity.java
@@ -1,9 +1,8 @@
 package com.cixtor.jhobber;
 
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
@@ -12,45 +11,40 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.ViewFlipper;
+
+import com.google.android.gms.maps.MapFragment;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
+
+    private Toolbar toolbar;
+    private DrawerLayout mDrawer;
+    private NavigationView nvDrawer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+
+        /* Set a Toolbar to replace the ActionBar */
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        final ViewFlipper vf = (ViewFlipper) this.findViewById(R.id.view_flipper);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Loading job posts...", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-                vf.setDisplayedChild(1);
-            }
-        });
-
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
-                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
-        drawer.addDrawerListener(toggle);
+        /* Find and attach a toggle event to our Drawer view */
+        mDrawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, mDrawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        mDrawer.addDrawerListener(toggle);
         toggle.syncState();
 
-        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
-        navigationView.setNavigationItemSelectedListener(this);
+        /* Setup Drawer view */
+        nvDrawer = (NavigationView) findViewById(R.id.nav_view);
+        nvDrawer.setNavigationItemSelectedListener(this);
     }
 
     @Override
     public void onBackPressed() {
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        if (drawer.isDrawerOpen(GravityCompat.START)) {
-            drawer.closeDrawer(GravityCompat.START);
+        if (mDrawer.isDrawerOpen(GravityCompat.START)) {
+            mDrawer.closeDrawer(GravityCompat.START);
         } else {
             super.onBackPressed();
         }
@@ -81,21 +75,47 @@ public class MainActivity extends AppCompatActivity
     @SuppressWarnings("StatementWithEmptyBody")
     @Override
     public boolean onNavigationItemSelected(MenuItem item) {
-        // Handle navigation view item clicks here.
-        int id = item.getItemId();
+        /* Create a new Fragment and specify the pieces to show */
+        Fragment fragment = null;
+        Class fragmentClass = null;
 
-        if (id == R.id.nav_home) {
+        /* Handle navigation view item clicks here. */
+        switch (item.getItemId()) {
+            case R.id.nav_home:
+                fragmentClass = HomeFragment.class;
+                break;
 
-        } else if (id == R.id.nav_map) {
+            case R.id.nav_map:
+                fragmentClass = MapFragment.class;
+                break;
 
-        } else if (id == R.id.nav_profile) {
+            case R.id.nav_profile:
+                fragmentClass = ProfileFragment.class;
+                break;
 
-        } else if (id == R.id.nav_settings) {
-
+            case R.id.nav_settings:
+                fragmentClass = SettingsFragment.class;
+                break;
         }
 
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        drawer.closeDrawer(GravityCompat.START);
+        /* abort invalid fragments */
+        if (fragmentClass == null) {
+            return false;
+        }
+
+        try {
+            fragment = (Fragment) fragmentClass.newInstance();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        fragmentManager.beginTransaction().replace(R.id.flContent, fragment).commit();
+
+        item.setChecked(true);
+        setTitle(item.getTitle());
+        mDrawer.closeDrawers();
+
         return true;
     }
 }
