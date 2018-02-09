7cbc49dc9c2a7b84dacee8b40d90846709c08d2a
Cixtor <cixtords@gmail.com>
2018-02-09 09:00:00 -0800

    Add signup implementation and flash messages on errors

--- app/src/main/java/com/cixtor/jhobber/fragment/Home.java
+++ app/src/main/java/com/cixtor/jhobber/fragment/Home.java
@@ -2,16 +2,34 @@ package com.cixtor.jhobber.fragment;
 
 import android.content.Context;
 import android.os.Bundle;
+import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
 import com.cixtor.jhobber.R;
+import com.cixtor.jhobber.activity.Base;
 
-public class Home extends Fragment {
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Home extends Fragment implements View.OnClickListener {
+    public final String TAG = "JHOBBER_HOME";
 
     private OnFragmentInteractionListener mListener;
+    private EditText username;
+    private Button signupBtn;
 
     public Home() {
     }
@@ -27,7 +45,14 @@ public class Home extends Fragment {
             mListener.onFragmentInteraction("Home");
         }
 
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        View v = inflater.inflate(R.layout.fragment_home, container, false);
+
+        this.username = (EditText) v.findViewById(R.id.username);
+        this.signupBtn = (Button) v.findViewById(R.id.signupBtn);
+
+        signupBtn.setOnClickListener(this);
+
+        return v;
     }
 
     @Override
@@ -44,6 +69,91 @@ public class Home extends Fragment {
     public void onDetach() {
         super.onDetach();
         mListener = null;
+    }
+
+    @Override
+    public void onClick(View v) {
+        int buttonId = v.getId();
+
+        try {
+            this.onClickSignup(v, buttonId);
+        } catch (JSONException e) {
+            Snackbar.make(
+                    getActivity().findViewById(R.id.flContent),
+                    e.getMessage(),
+                    Snackbar.LENGTH_SHORT
+            ).show();
+        }
+    }
+
+    private void onClickSignup(View v, int buttonId) throws JSONException {
+        if (buttonId != R.id.signupBtn) {
+            return;
+        }
+
+        Base parent = (Base) this.getActivity();
+
+        StringRequest obj = new StringRequest(
+                Request.Method.POST,
+                parent.WEB_SERVICE + "/signup",
+                this.onClickSignupResponseListener(parent),
+                this.onClickSignupErrorListener()
+        ) {
+            @Override
+            protected Map<String, String> getParams() {
+                Map<String, String> params = new HashMap<String, String>();
+                params.put("username", username.getText().toString());
+                return params;
+            }
+
+            @Override
+            public Map<String, String> getHeaders() throws AuthFailureError {
+                Map<String, String> params = new HashMap<String, String>();
+                params.put("Content-Type", "application/x-www-form-urlencoded");
+                return params;
+            }
+        };
+
+        parent.addRequestToQueue(obj);
+    }
+
+    public Response.Listener<String> onClickSignupResponseListener(final Base parent) {
+        return new Response.Listener<String>() {
+            @Override
+            public void onResponse(String out) {
+                String message;
+
+                try {
+                    JSONObject res = new JSONObject(out);
+
+                    if (res.getBoolean("ok")) {
+                        /* user registration was successful */
+                        message = "Account successfully created.";
+                        parent.setUserAccount(res);
+                    } else {
+                        /* user registration failed */
+                        message = res.getString("error");
+                    }
+                } catch (JSONException e) {
+                    message = e.getMessage();
+                }
+
+                Snackbar.make(
+                        getActivity().findViewById(R.id.flContent),
+                        message,
+                        Snackbar.LENGTH_SHORT
+                ).show();
+            }
+        };
+    }
+
+    public Response.ErrorListener onClickSignupErrorListener() {
+        return new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                // TODO: Handle error
+            }
+        };
     }
 
     public interface OnFragmentInteractionListener {
