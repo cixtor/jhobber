1dc5d1bb6abfcf368a23e2a4e9d765b92205695a
Cixtor <cixtords@gmail.com>
2018-02-19 09:00:00 -0800

    Add more attributes to the job model for future-proof

--- app/src/main/java/com/cixtor/jhobber/model/Job.java
+++ app/src/main/java/com/cixtor/jhobber/model/Job.java
@@ -1,21 +1,32 @@
 package com.cixtor.jhobber.model;
 
 public class Job {
-    private int mID;
+    private String mID;
     private String mCompany;
-    private String mImage;
-    private String mOccupation;
-    private String mDescription;
+    private String mTitle;
+    private String mSummary;
     private String mSkills;
+    private String mSource;
+    private String mImage;
+    private String mContact;
+    private String mKind;
+    private boolean mActive;
+    private boolean mRemote;
+    private boolean mVisa;
+    private String mCountry;
+    private String mCity;
+    private double mLongitude;
+    private double mLatitude;
+    private String mDatetime;
 
     public Job() {
     }
 
-    public int getID() {
+    public String getID() {
         return mID;
     }
 
-    public void setID(int ID) {
+    public void setID(String ID) {
         mID = ID;
     }
 
@@ -27,28 +38,20 @@ public class Job {
         mCompany = company;
     }
 
-    public String getImage() {
-        return mImage;
+    public String getTitle() {
+        return mTitle;
     }
 
-    public void setImage(String image) {
-        mImage = image;
+    public void setTitle(String title) {
+        mTitle = title;
     }
 
-    public String getOccupation() {
-        return mOccupation;
+    public String getSummary() {
+        return mSummary;
     }
 
-    public void setOccupation(String occupation) {
-        mOccupation = occupation;
-    }
-
-    public String getDescription() {
-        return mDescription;
-    }
-
-    public void setDescription(String description) {
-        mDescription = description;
+    public void setSummary(String summary) {
+        mSummary = summary;
     }
 
     public String getSkills() {
@@ -59,8 +62,104 @@ public class Job {
         mSkills = skills;
     }
 
+    public String getSource() {
+        return mSource;
+    }
+
+    public void setSource(String source) {
+        mSource = source;
+    }
+
+    public String getImage() {
+        return mImage;
+    }
+
+    public void setImage(String image) {
+        mImage = image;
+    }
+
+    public String getContact() {
+        return mContact;
+    }
+
+    public void setContact(String contact) {
+        mContact = contact;
+    }
+
+    public String getKind() {
+        return mKind;
+    }
+
+    public void setKind(String kind) {
+        mKind = kind;
+    }
+
+    public boolean isActive() {
+        return mActive;
+    }
+
+    public void setActive(boolean active) {
+        mActive = active;
+    }
+
+    public boolean isRemote() {
+        return mRemote;
+    }
+
+    public void setRemote(boolean remote) {
+        mRemote = remote;
+    }
+
+    public boolean isVisa() {
+        return mVisa;
+    }
+
+    public void setVisa(boolean visa) {
+        mVisa = visa;
+    }
+
+    public String getCountry() {
+        return mCountry;
+    }
+
+    public void setCountry(String country) {
+        mCountry = country;
+    }
+
+    public String getCity() {
+        return mCity;
+    }
+
+    public void setCity(String city) {
+        mCity = city;
+    }
+
+    public double getLongitude() {
+        return mLongitude;
+    }
+
+    public void setLongitude(double longitude) {
+        mLongitude = longitude;
+    }
+
+    public double getLatitude() {
+        return mLatitude;
+    }
+
+    public void setLatitude(double latitude) {
+        mLatitude = latitude;
+    }
+
+    public String getDatetime() {
+        return mDatetime;
+    }
+
+    public void setDatetime(String datetime) {
+        mDatetime = datetime;
+    }
+
     @Override
     public String toString() {
-        return "Job{company='" + mCompany + "', jobtitle='" + mOccupation + "'}";
+        return "Job{company='" + mCompany + "', jobtitle='" + mTitle + "'}";
     }
 }
